2d56e94cd3a58c8cc38e94cbbe3c07c07e7e153e
steve.nadalin <steve.nadalin@he-arc.ch>
2017-01-10 17:18:19 +0100

    continuation lex.py et ajout test.py

--- Projet/lex.py
+++ Projet/lex.py
@@ -1,5 +1,55 @@
 import ply.lex as lex
 
 tokens = (
-
+    'DEBUT_PROGRAMME', 'FIN_PROGRAMME', 'FIN_LIGNE', 'DEBUT_LIGNE', 'TEXTE', "ATTRIBUT", 'EGAL',"VALEUR"
 )
+
+def t_DEBUT_PROGRAMME(t):
+        r'\<svg'
+        return t
+
+def t_FIN_PROGRAMME(t):
+        r'\<\/svg'
+        return t
+
+def t_FIN_LIGNE(t):
+        r'\>'
+        t.lexer.lineno += len(t.value)
+        return t
+
+def t_EGAL(t):
+        r'\='
+        return t
+
+def t_DEBUT_LIGNE(t):
+        r'\<'
+        return t
+
+def t_TEXTE(t):
+        r'[A-Za-z]+'
+        return t
+
+def t_VALEUR(t):
+        r'\"\d+\.?\d*[A-Za-z]*\"'
+        t.value = t.value.replace('"','')
+        t.value = t.value.replace('px', '')
+        return t
+
+t_ignore = ' \n /'
+
+def t_error(t):
+        print("Illegal caracter '%s'" % t.value[0])
+        t.lexer.skip(1)
+
+lex.lex()
+
+if __name__ == "__main__":
+    import sys
+    prog = open(sys.argv[1]).read()
+
+    lex.input(prog)
+
+    while 1:
+        tok = lex.token()
+        if not tok: break
+        print("line %d: %s(%s)" % (tok.lineno, tok.type, tok.value))
